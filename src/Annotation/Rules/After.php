4774563f11c1bb3979e348df1de59da7a51e4397
WIN-20251231REK <WIN-20251231REK@qq.com>
2026-03-23 13:50:31 +0800

    修复调用错误方法的bug

---
+++ src/Annotation/Rules/After.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Kooditorm\Validation\Annotation\Rules;
+
+use Attribute;
+use Kooditorm\Validation\Annotation\ValidatorAnnotation;
+
+/**
+ * 验证字段必须是给定日期之后的一个值，日期将会通过 PHP 函数 strtotime 传递：
+ *
+ * 'start_date' => 'required|date|after:tomorrow'
+ *
+ * 你可以指定另外一个与日期进行比较的字段，而不是传递一个日期字符串给 strtotime 执行：
+ *
+ * 'finish_date' => 'required|date|after:start_date'
+ */
+#[Attribute(Attribute::TARGET_PROPERTY | Attribute::IS_REPEATABLE)]
+class After extends ValidatorAnnotation
+{
+
+    public function __construct(public string $value, public string $message = '')
+    {
+    }
+
+    public function rule():string
+    {
+        return 'after:{value}';
+    }
+}
---
+++ src/Annotation/Rules/AfterOrEqual.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Kooditorm\Validation\Annotation\Rules;
+
+use Attribute;
+use Kooditorm\Validation\Annotation\ValidatorAnnotation;
+
+/**
+ * 验证字段必须是大于等于给定日期的值，更多信息，请参考 after:date 规则。
+ */
+#[Attribute(Attribute::TARGET_PROPERTY | Attribute::IS_REPEATABLE)]
+class AfterOrEqual extends ValidatorAnnotation
+{
+    public function __construct(public string $value, public string $message = '')
+    {
+    }
+
+    public function rule(): string
+    {
+        return 'after_or_equal';
+    }
+}
