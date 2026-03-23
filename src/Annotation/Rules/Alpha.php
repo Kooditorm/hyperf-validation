fb8091c3d108f314235afb683b1a5dee801abe06
WIN-20251231REK <WIN-20251231REK@qq.com>
2026-03-23 14:18:43 +0800

    修复调用错误方法的bug

---
+++ src/Annotation/Rules/Alpha.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Kooditorm\Validation\Annotation\Rules;
+
+use Attribute;
+use Kooditorm\Validation\Annotation\ValidatorAnnotation;
+
+/**
+ * 验证字段必须是字母(包含中文)。 为了将此验证规则限制在 ASCII 范围内的字符（a-z 和 A-Z），你可以为验证规则提供 ascii 选项：
+ */
+#[Attribute(Attribute::TARGET_PROPERTY | Attribute::IS_REPEATABLE)]
+class Alpha extends ValidatorAnnotation
+{
+    public function __construct(public string $message = '')
+    {
+
+    }
+
+    public function rule(): string
+    {
+        return 'alpha:{value}';
+    }
+}
