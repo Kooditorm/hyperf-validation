e2818660a8f4a32ac550d62a63fa19270c97da3c
WIN-20251231REK <WIN-20251231REK@qq.com>
2026-03-23 16:01:46 +0800

    修复调用错误方法的bug

--- src/Annotation/Rules/AlphaDash.php
+++ src/Annotation/Rules/AlphaDash.php
@@ -3,18 +3,15 @@ declare(strict_types=1);
 
 namespace Kooditorm\Validation\Annotation\Rules;
 
+
 use Attribute;
-use Kooditorm\Validation\Annotation\ValidatorAnnotation;
 
 #[Attribute(Attribute::TARGET_PROPERTY | Attribute::IS_REPEATABLE)]
-class Accepted extends ValidatorAnnotation
+class AlphaDash extends Alpha
 {
 
-    public function __construct(public string $message = '')
-    {
-    }
     public function rule():string
     {
-        return 'accepted';
+        return 'alpha_dash:{value}';
     }
 }
--- src/Annotation/Rules/Declined.php
+++ src/Annotation/Rules/Declined.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace Kooditorm\Validation\Annotation\Rules;
 
